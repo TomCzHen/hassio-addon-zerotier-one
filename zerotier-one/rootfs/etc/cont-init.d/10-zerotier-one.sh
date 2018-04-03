5785f9c42f80ec168ad9943a509b5b81c3258c24
Tom CzHen <tom.czhen@gmail.com>
2018-04-04 01:04:09 +0800

    增加 api 端口选项

--- zerotier-one/rootfs/etc/cont-init.d/10-zerotier-one.sh
+++ zerotier-one/rootfs/etc/cont-init.d/10-zerotier-one.sh
@@ -6,16 +6,14 @@
 source /usr/lib/hassio-addons/base.sh
 
 if [ ! -d "/data/networks.d" ]; then
-    mkdir -m a=rwx /data/networks.d
+    mkdir /data/networks.d
 fi
 
-network_id=$(hass.config.get 'network')
+network=$(hass.config.get 'network')
 
-if [ ! -f "/data/networks.d/${network_id}.conf" ] && [ ${network_id} ]; then
+if [ ! -f "/data/networks.d/${network}.conf" ] && [ ${network} ]; then
     rm -f /data/networks.d/*
-    touch "/data/networks.d/${network_id}.conf"
+    touch "/data/networks.d/${network}.conf"
 fi
-
-#chown -R zerotier-one:zerotier-one /data
 
 ln -s /data /var/lib/zerotier-one
